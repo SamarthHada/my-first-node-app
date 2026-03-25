03c45c33b0063cb4fe41543a8b6db507ab90efef
Samarth Hada <samarthbshada@gmail.com>
2026-03-25 17:26:16 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello from Vercel');
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
